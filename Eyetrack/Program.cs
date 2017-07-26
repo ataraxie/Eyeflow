69cf89964e0dd1d52f148bfb10c38eca390b1dad
Felix Grund <felix.grund@scandio.de>
2017-07-25 17:50:18 -0700

    .

--- Eyetrack/Program.cs
+++ Eyetrack/Program.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using Eyetrack.Runners;
 //using Eyetrack.Runners.TobiiWelcomeExample;
 using Eyetrack.Runners.WebsocketExample;
 //using Eyetrack.Runners.GazeWindows;
 //using Eyetrack.Runners.RectangleDrawer;
-//using Eyetrack.Runners.WindowHighlight;
+using Eyetrack.Runners.WindowHighlight;
 using Eyetrack.Runners.WindowIdentifier;
 using Eyetrack.Runners.WindowBlur;
-using Eyetrack.Runners.RectangleRemover;
-using Eyetrack.Runners.SocketExample;
+//using Eyetrack.Runners.RectangleRemover;
+//using Eyetrack.Runners.SocketExample;
+using Eyetrack.Runners.AutoHotkeyTest;
 
 namespace Eyetrack
 {
@@ -26,10 +28,13 @@ namespace Eyetrack
             //runner = new GazeWindowsRunner();
             //runner = new RectangleDrawerRunner();
             //runner = new WindowHighlightRunner();
-            runner = new WindowIdentifierRunner();
+            //runner = new WindowIdentifierRunner();
             //runner = new WindowBlurRunner();
             //runner = new RectangleRemover();
             //runner = new SocketRunner();
+            //runner = new AutoHotkeyTestRunner();
+            runner = new AhkWindowHighlightRunner();
+            //runner = new AhkWindowHighlightRunner3();
             runner.run();
         }
     }
